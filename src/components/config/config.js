d079cdf28c36725da938402fb28cf907999d22f7
chengchinlim <chengchin5227@gmail.com>
2019-09-13 16:07:15 -0700

    Testing branching

---
+++ src/components/config/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDWg0loTuiOEsk7a39iDpqW4eT9cjOUiiM",
+    authDomain: "divevr-fec3b.firebaseapp.com",
+    databaseURL: "https://divevr-fec3b.firebaseio.com",
+    projectId: "divevr-fec3b",
+    storageBucket: "",
+    messagingSenderId: "1085364079236",
+    appId: "1:1085364079236:web:12323ff403073a62"
+  };
